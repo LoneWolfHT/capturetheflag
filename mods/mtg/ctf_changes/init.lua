6cd7c2ac004f0b8cdcdb6c78c3273cfaa60bfdd3
LoneWolfHT <lonewolf04361@gmail.com>
2021-06-26 18:23:04 -0700

    Update init.lua

--- mods/mtg/ctf_changes/init.lua
+++ mods/mtg/ctf_changes/init.lua
@@ -67,102 +67,14 @@ minetest.register_on_mods_loaded(function()
 
 		minetest.override_item(name, {groups = def.groups, _g_category = new_category})
 	end
+
+	local drop_self = {
+		"default:leaves", "default:jungleleaves", "default:acacia_leaves",
+		"default:aspen_leaves", "default:bush_leaves", "default:blueberry_bush_leaves",
+		"default:acacia_bush_leaves"
+	}
+
+	for _, name in pairs(drop_self) do
+		minetest.override_item(name, {drop = name})
+	end
 end)
-
---leaves
-minetest.register_node(":default:leaves", {
-	description = "Apple Tree Leaves",
-	drawtype = "allfaces_optional",
-	waving = 1,
-	tiles = {"default_leaves.png"},
-	special_tiles = {"default_leaves_simple.png"},
-	paramtype = "light",
-	is_ground_content = false,
-	groups = {snappy = 3, leafdecay = 3, flammable = 2, leaves = 1},
-	sounds = default.node_sound_leaves_defaults(),
-
-	after_place_node = after_place_leaves,
-})
-
-minetest.register_node(":default:jungleleaves", {
-	description = "Jungle Tree Leaves",
-	drawtype = "allfaces_optional",
-	waving = 1,
-	tiles = {"default_jungleleaves.png"},
-	special_tiles = {"default_jungleleaves_simple.png"},
-	paramtype = "light",
-	is_ground_content = false,
-	groups = {snappy = 3, leafdecay = 3, flammable = 2, leaves = 1},
-	sounds = default.node_sound_leaves_defaults(),
-
-	after_place_node = after_place_leaves,
-})
-
-minetest.register_node(":default:acacia_leaves", {
-	description = "Acacia Tree Leaves",
-	drawtype = "allfaces_optional",
-	tiles = {"default_acacia_leaves.png"},
-	special_tiles = {"default_acacia_leaves_simple.png"},
-	waving = 1,
-	paramtype = "light",
-	is_ground_content = false,
-	groups = {snappy = 3, leafdecay = 3, flammable = 2, leaves = 1},
-	sounds = default.node_sound_leaves_defaults(),
-
-	after_place_node = after_place_leaves,
-})
-
-minetest.register_node(":default:aspen_leaves", {
-	description = "Aspen Tree Leaves",
-	drawtype = "allfaces_optional",
-	tiles = {"default_aspen_leaves.png"},
-	waving = 1,
-	paramtype = "light",
-	is_ground_content = false,
-	groups = {snappy = 3, leafdecay = 3, flammable = 2, leaves = 1},
-	sounds = default.node_sound_leaves_defaults(),
-
-	after_place_node = after_place_leaves,
-})
-
-minetest.register_node(":default:bush_leaves", {
-	description = "Bush Leaves",
-	drawtype = "allfaces_optional",
-	tiles = {"default_leaves_simple.png"},
-	paramtype = "light",
-	groups = {snappy = 3, flammable = 2, leaves = 1},
-	sounds = default.node_sound_leaves_defaults(),
-
-	after_place_node = after_place_leaves,
-})
-
-minetest.register_node(":default:blueberry_bush_leaves", {
-	description = "Blueberry Bush Leaves",
-	drawtype = "allfaces_optional",
-	tiles = {"default_blueberry_bush_leaves.png"},
-	paramtype = "light",
-	groups = {snappy = 3, flammable = 2, leaves = 1},
-	drop = "default:blueberry_bush_leaves",
-	sounds = default.node_sound_leaves_defaults(),
-	on_timer = function(pos, elapsed)
-		if minetest.get_node_light(pos) < 11 then
-			minetest.get_node_timer(pos):start(200)
-		else
-			minetest.set_node(pos, {name = "default:blueberry_bush_leaves_with_berries"})
-		end
-	end,
-
-	after_place_node = after_place_leaves,
-})
-
-minetest.register_node(":default:acacia_bush_leaves", {
-	description = "Acacia Bush Leaves",
-	drawtype = "allfaces_optional",
-	tiles = {"default_acacia_leaves_simple.png"},
-	paramtype = "light",
-	groups = {snappy = 3, flammable = 2, leaves = 1},
-	drop = "default:acacia_bush_leaves",
-	sounds = default.node_sound_leaves_defaults(),
-
-	after_place_node = after_place_leaves,
-})
