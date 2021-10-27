1cb246dc0d02956473e029ce50d8d6ba03a60126
LoneWolfHT <lonewolf04361@gmail.com>
2021-10-27 14:20:24 -0700

    Fix mod security crash

--- mods/ctf/ctf_rankings/init.lua
+++ mods/ctf/ctf_rankings/init.lua
@@ -1,14 +1,17 @@
 local backend = minetest.settings:get("ctf_rankings_backend") or "default"
 
 local rankings
-local env
 local top = ctf_core.include_files("top.lua")
 
 if backend == "redis" then
-	env = minetest.request_insecure_environment()
+	local env = minetest.request_insecure_environment()
 	assert(env, "Please add 'ctf_rankings' to secure.trusted_mods if you want to use the redis backend")
 
+	local old_require = require
+
+	env.rawset(_G, "require", env.require)
 	rankings = env.dofile(env.core.get_modpath(env.core.get_current_modname()).."/redis.lua")
+	env.rawset(_G, "require", old_require)
 else
 	rankings = ctf_core.include_files(backend..".lua")
 end
